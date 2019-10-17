915d044e8b85b3ab024a105ec74b5622974922c6
wgx731 <wgx731@gmail.com>
2019-10-17 21:27:29 +0800

    [DEV]: alphabet-cipher done

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,31 @@
 (ns alphabet-cipher.coder)
 
+(def alphabets [\a \b \c \d \e \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z])
+(defn get-alphabet-index [item] (.indexOf alphabets item))
+(defn encode-alphabet [col row] (last (take (+ row 1) (drop col (cycle alphabets)))))
+(defn decode-alphabet [col res] (get alphabets (.indexOf (take 26 (drop col (cycle alphabets))) res)))
+(defn extract-key-alphabet [res row] (get alphabets (.indexOf (take 26 (drop row (cycle alphabets))) res)))
+(defn first-seq [coll n]
+  (if (apply = (partition n coll))
+    (first (partition n coll))
+    (recur coll (+ n 1))
+    ))
+
 (defn encode [keyword message]
-  "encodeme")
+  (let [k (map get-alphabet-index (take (count message) (cycle keyword)))
+        m (map get-alphabet-index message)]
+    (apply str (map encode-alphabet k m))
+    ))
 
 (defn decode [keyword message]
-  "decodeme")
+  (let [k (map get-alphabet-index (take (count message) (cycle keyword)))
+        m message]
+    (apply str (map decode-alphabet k m))
+    ))
 
 (defn decipher [cipher message]
-  "decypherme")
+  (let [c cipher
+        m (map get-alphabet-index message)]
+    (apply str (first-seq (map extract-key-alphabet c m) 1))
+    ))
 
