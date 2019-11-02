981f9e3c20d782fc1c9ba73f28e4e385e387c014
wgx731 <wgx731@gmail.com>
2019-11-02 21:30:54 +0800

    [DEV]: day 18 clojure

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,13 @@
 (ns wonderland-number.finder)
 
+(defn same-digits? [a b]
+  (= (sort (str a)) (sort (str b))))
+
+(defn wonderland-numbers []
+  (letfn [(f [x] (->> (map #(* x %) [2 3 4 5 6])
+                 (every? #(same-digits? x %))))]
+    (->> (range 100000 1000000)
+         (filter f))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (wonderland-numbers)))
