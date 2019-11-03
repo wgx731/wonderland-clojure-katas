f35d554ebc80c1966e7102f4e86f874e92ccc865
wgx731 <wgx731@gmail.com>
2019-11-03 21:12:08 +0800

    [DEV]: day 19 practice

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -2,5 +2,21 @@
 
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
+
 (defn river-crossing-plan []
-  start-pos)
+  '((#{:you :fox :goose :corn} #{:boat} #{})
+    (#{:fox :corn} #{:you :boat :goose} #{})
+    (#{:fox :corn} #{:boat} #{:you :goose})
+    (#{:fox :corn} #{:you :boat} #{:goose})
+    (#{:you :fox :corn} #{:boat} #{:goose})
+    (#{:corn} #{:you :fox :boat} #{:goose})
+    (#{:corn} #{:boat} #{:you :fox :goose})
+    (#{:corn} #{:you :boat :goose} #{:fox})
+    (#{:you :goose :corn} #{:boat} #{:fox})
+    (#{:goose} #{:you :boat :corn} #{:fox})
+    (#{:goose} #{:boat} #{:you :fox :corn})
+    (#{:goose} #{:you :boat} #{:fox :corn})
+    (#{:you :goose} #{:boat} #{:fox :corn})
+    (#{} #{:you :boat :goose} #{:fox :corn})
+    (#{} #{:boat} #{:you :fox :goose :corn})))
+
