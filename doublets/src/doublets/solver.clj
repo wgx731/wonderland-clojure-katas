751a1f71b05e895528cf30bfba2ca13b8bdeacb5
wgx731 <wgx731@gmail.com>
2019-11-04 22:13:22 +0800

    [DEV]: day 20 practice

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -1,11 +1,39 @@
 (ns doublets.solver
-  (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]))
+  (:require [clojure.java.io :as io-lib]))
 
 (def words (-> "words.edn"
-               (io/resource)
+               (io-lib/resource)
                (slurp)
                (read-string)))
 
+(defn- char-diff-count [w1 w2]
+  (get (frequencies (map = w1 w2)) false 0))
+
+(defn- next-words [word]
+  (->> (set words)
+       (filter #(= 1 (char-diff-count word %)))))
+
+;; TODO: write main doublets function
+
+(defn- hamming-distance [x y]
+  (and
+    (= (count x) (count y))
+    (->> [x y]
+         (apply map not=)
+         (filter identity)
+         count)))
+
+(defn- next-steps [dict x]
+  (map
+    #(conj x %)
+    (filter
+      #(= 1 (hamming-distance (last x) %))
+      dict)))
+
 (defn doublets [word1 word2]
-  "make me work")
+  (loop [path [[word1]] n (count words)]
+    (let [matching-path (filter #(some #{word2} %) path)]
+      (or
+        (first matching-path)
+        (if (< n 0) [])
+        (recur (apply concat (map #(next-steps words %) path)) (dec n))))))
