d20d13a7fce395db49460cb5e99587488b03144f
wgx731 <wgx731@gmail.com>
2019-11-07 21:45:49 +0800

    [DEV]: day 23 practice

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,13 +1,39 @@
 (ns card-game-war.game)
 
-;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
-(def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
+(def ranks [2 3 4 5 6 7 8 9 :jack :queen :king :ace])
 (def cards
-  (for [suit suits
-        rank ranks]
-    [suit rank]))
+  (for [suit suits rank ranks] [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(def numeric-ranks {2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 :jack 10 :queen 11 :king 12 :ace 13
+                    :spade 1 :club 2 :diamond 3 :heart 4})
 
-(defn play-game [player1-cards player2-cards])
+(defn numeric-rank
+  [k]
+  (get numeric-ranks k 0))
+
+(defn play-round
+  [player1-card player2-card]
+  (let [[player1-suit-rank player1-rank] (map numeric-rank player1-card)
+        [player2-suit-rank player2-rank] (map numeric-rank player2-card)]
+    (cond
+      (> player1-rank player2-rank) :player1
+      (< player1-rank player2-rank) :player2
+      (> player1-suit-rank player2-suit-rank) :player1
+      :else :player2)))
+
+(defn play-game
+  [player1-cards player2-cards]
+  (loop [player1-cards player1-cards
+         player2-cards player2-cards]
+    (cond
+      (nil? (seq player2-cards)) :player1-wins
+      (nil? (seq player1-cards)) :player2-wins
+      :else
+      (let [player1-card (first player1-cards)
+            player2-card (first player2-cards)]
+        (condp = (play-round player1-card player2-card)
+          :player1 (recur (concat (rest player1-cards) [player1-card player2-card])
+                          (rest player2-cards))
+          :player2 (recur (rest player1-cards)
+                          (concat (rest player2-cards) [player2-card player1-card])))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -2,18 +2,74 @@
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
-
-;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= :player1 (play-round [:heart 4] [:heart 3])))
+    (is (= :player1 (play-round [:club 4] [:heart 3])))
+    (is (= :player1 (play-round [:spade 4] [:spade 3])))
+    (is (= :player1 (play-round [:diamond 4] [:diamond 3]))))
+  (testing "queens are higer rank than jacks"
+    (is (= :player1 (play-round [:heart :queen] [:heart :jack]))))
+  (testing "kings are higer rank than queens"
+    (is (= :player1 (play-round [:heart :king] [:heart :queen]))))
+  (testing "aces are higer rank than kings"
+    (is (= :player1 (play-round [:heart :ace] [:heart :king]))))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= :player1 (play-round [:club :ace] [:spade :ace]))))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= :player1 (play-round [:diamond :ace] [:club :ace]))))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= :player1 (play-round [:heart :ace] [:diamond :ace])))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
-
+  (testing "the player loses when they run out of cards"
+    (is (= :player2-wins (play-game [[:spade 4] [:heart :jack]]
+                                    [[:heart 4] [:spade :ace]])))
+    (is (= :player1-wins (play-game [[:club :ace]
+                                     [:club :jack]
+                                     [:spade 8]
+                                     [:diamond 3]
+                                     [:diamond :queen]
+                                     [:spade :ace]
+                                     [:club 2]
+                                     [:club 7]
+                                     [:heart 7]
+                                     [:spade 4]
+                                     [:heart 3]
+                                     [:heart 9]
+                                     [:heart :queen]
+                                     [:spade 7]
+                                     [:diamond :jack]
+                                     [:heart :ace]
+                                     [:diamond 8]
+                                     [:club 4]
+                                     [:club 9]
+                                     [:diamond 9]
+                                     [:club 3]
+                                     [:heart 8]
+                                     [:heart :king]
+                                     [:spade 2]]
+                                    [[:club :king]
+                                     [:spade :king]
+                                     [:diamond 5]
+                                     [:spade 9]
+                                     [:spade 6]
+                                     [:club 5]
+                                     [:club :queen]
+                                     [:heart 6]
+                                     [:diamond 6]
+                                     [:heart :jack]
+                                     [:spade 3]
+                                     [:diamond :king]
+                                     [:heart 4]
+                                     [:heart 5]
+                                     [:diamond 2]
+                                     [:spade :queen]
+                                     [:spade :jack]
+                                     [:club 6]
+                                     [:diamond 4]
+                                     [:diamond 7]
+                                     [:heart 2]
+                                     [:diamond :ace]
+                                     [:club 8]
+                                     [:spade 5]])))))
